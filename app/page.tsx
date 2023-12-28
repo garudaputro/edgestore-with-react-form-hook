704f2339736e1edb5d5bc13db78ef3ef2b6f11e0
garudaputro <garudaputro@gmail.com>
2023-12-28 08:23:12 -0800

    added single image upload edgestore

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,183 @@
-import Image from 'next/image'
+// Let me tell you a little about what I want to make, I want to make a form that is used to upload product images and data about that product
+// previously I used react hook form to create a form in nextjs, then I wanted to upload the product image using the edgestore that you made, I also used shadcn ui for styling
+// first import what i need
 
-export default function Home() {
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { SingleImageDropzone } from "@/components/upload/single-image";
+import { useEdgeStore } from "@/lib/edgestore";
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import PulseLoader from "react-spinners/PulseLoader";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "name must be at least 2 characters.",
+  }),
+});
+
+export default function TestUploadWithForm() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className="flex justify-center items-center h-screen">
+      <SingleImageExample />
+    </div>
+  );
 }
+
+//  and this is my component base on docs of edgestore about uploading image
+
+function SingleImageExample() {
+  const [file, setFile] = useState<File>();
+  const [progress, setProgress] = useState<
+    "PENDING" | "COMPLETE" | "ERROR" | number
+  >("PENDING");
+  const [uploadRes, setUploadRes] = useState<{
+    url: string;
+    filename: string;
+  }>();
+  const { edgestore } = useEdgeStore();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    if (file) {
+      try {
+        setIsSubmitting(true);
+        const res = await edgestore.myPublicImages.upload({
+          file,
+          input: { type: "product" },
+          onProgressChange: async (newProgress) => {
+            setProgress(newProgress);
+            if (newProgress === 100) {
+              // wait 1 second to set it to complete
+              // so that the user can see it at 100%
+              await new Promise((resolve) => setTimeout(resolve, 1000));
+              setProgress("COMPLETE");
+            }
+          },
+        });
+        setUploadRes({
+          url: res.url,
+          filename: file.name,
+        });
+
+        // check value of uploadRes bug always got undefined
+        console.log(uploadRes);
+        console.log(values);
+
+        //  i want to send the data to this api, im using prisma on this , while I give a comment
+
+        // const response = await axios.post("/api/products", {
+        //   thumbnail: uploadRes?.url,
+        //   name: values.name,
+        // });
+
+        // if (response.status === 201) {
+        //   toast.success(response.data.message);
+        // }
+      } catch (err) {
+        setProgress("ERROR");
+      }
+    }
+    setIsSubmitting(false);
+  };
+
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">Add Data</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px] ">
+          <DialogHeader>
+            <DialogTitle>Add Data</DialogTitle>
+            <DialogDescription>insert data product</DialogDescription>
+          </DialogHeader>
+          <div className=" max-h-screen  ">
+            <div className="flex flex-col items-center">
+              <SingleImageDropzone
+                height={200}
+                width={200}
+                value={file}
+                onChange={setFile}
+                disabled={progress !== "PENDING"}
+                dropzoneOptions={{
+                  maxSize: 1024 * 1024 * 1, // 1 MB
+                }}
+              />
+
+              {uploadRes && (
+                <div className="mt-2">
+                  <a
+                    href={uploadRes.url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="underline"
+                  >
+                    {uploadRes.filename}
+                  </a>
+                </div>
+              )}
+            </div>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>product name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="product" {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button className="w-full my-5" size={"lg"} type="submit">
+                  {isSubmitting ? (
+                    <PulseLoader color="#fff" size={10} />
+                  ) : (
+                    "Submit"
+                  )}
+                </Button>
+              </form>
+            </Form>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
+
+// what i want is, upload thumbnail product with name of ProductDetails, but i got empty string
+// i've searching on google but still have no idea about it, would you gimme example usinf edgestore with react hook form? i think this is will be great tutorial if you make it
